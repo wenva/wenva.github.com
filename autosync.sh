9e139f75e82980e3034cafea332d2c9cf64db975
smallmuou <lvyexuwenfa100@126.com>
2014-12-01 20:48:01 +0800

    sync

---
+++ autosync.sh
@@ -0,0 +1,3 @@
+git add --all .                                                                  
+git commit -m 'sync'                                                             
+git push origin master 
